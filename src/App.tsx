da2c37c1506af35e23616fc579f41d40b30557fd
ga-reth <hi@gareth-v.com>
2025-04-01 12:06:57 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import viteLogo from '/vite.svg'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
